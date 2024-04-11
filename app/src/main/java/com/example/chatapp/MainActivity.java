f706d79d14d4e5d3543d9a11c3378b704af6171d
LordGuenni <florian.stamer@outlook.de>
2024-04-11 08:50:48 +0200

    First Commit

---
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.chatapp;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import de.fhb.fbi.msr.maus.uebung1.chatapp.AbstractMessageSender;
+import de.fhb.fbi.msr.maus.uebung1.chatapp.PushClient;
+
+import java.io.PrintWriter;
+
+public class MainActivity extends AbstractMessageSender {
+
+    private class IOHandler implements PushClient.IOHandler {
+        private PrintWriter writer;
+        @Override
+        public void displayInput(String input) {
+            updateConversation(input);
+        }
+
+        @Override
+        public void sendOutput(String output) {
+            // writer.write(output);
+            writer.println(output);
+        }
+
+        @Override
+        public void setOutputWriter(PrintWriter writer) {
+            this.writer = writer;
+        }
+    }
+    private IOHandler ioHandler = new IOHandler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PushClient client = new PushClient(ioHandler);
+        client.connect();
+    }
+
+    @Override
+    protected void setContentView() {
+        setContentView(R.layout.conversation);
+    }
+
+    @Override
+    protected void processMessage(String message) {
+        super.updateConversation(message);
+        ioHandler.sendOutput(message);
+    }
+}
